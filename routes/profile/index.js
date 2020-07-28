9e0f84b1f2bc53a4be265d45a54c86c29826e761
Ado Dragicevic <adodragicevic@gmail.com>
2020-07-28 18:17:03 +0000

    add user profile delete route

--- routes/profile/index.js
+++ routes/profile/index.js
@@ -11,8 +11,8 @@ const Gallery = require("../../models/gallery/gallery");
 router.get("/:id", async function(req, res){
 	try{
 		let user = await User.findById(req.params.id);
-		let blogs = await Blog.find().where("author.username").equals(user.username).exec();
-		let galleries = await Gallery.find().where("author.username").equals(user.username).exec();
+		let blogs = await Blog.find().where("author.id").equals(user._id).exec();
+		let galleries = await Gallery.find().where("author.id").equals(user._id).exec();
 		res.render("profile/show", {user: user, blogs: blogs, galleries: galleries});
 	}catch(err){
 		console.log(err);
@@ -20,8 +20,45 @@ router.get("/:id", async function(req, res){
 	}
 });
 
+// DELETE warning page
+router.get("/:id/delete", function(req, res){
+	User.findById(req.params.id, function(err, user){
+		if(err){
+			console.log(err);
+			req.flash("error", "Unable to execute request. Try again later.");
+			req.redirect("/");
+		}else{
+			res.render("profile/delete", {user: user});
+		}
+	});
+});
 
-
+// DELETE
+router.delete("/:id", async function(req, res){
+	try{
+		let user = await User.findById(req.params.id);
+		let blogs = await Blog.find().where("author.id").equals(user._id).populate("comments").exec();
+		let galleries = await Gallery.find().where("author.id").equals(user._id).exec();
+		await blogs.forEach(function(blog){
+			blog.comments.forEach(function(comment){
+				comment.delete();
+			});
+		});
+		await blogs.forEach(function(blog){
+				blog.delete();
+		});
+		await galleries.forEach(function(gallery){
+			gallery.delete();
+		});
+		user.delete();
+		req.flash("success", "User data successfully deleted.");
+		res.redirect("/");
+	}catch(err){
+		console.log(err);
+		req.flash("error", "Something went wrong. Please, try again later");
+		res.redirect("back");
+	}
+});
 
 
 module.exports = router;
