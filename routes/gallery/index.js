727a5b655b6512422043bd29105c0f824b6fe517
Ado Dragicevic <adodragicevic@gmail.com>
2020-07-28 08:26:38 +0000

    changing the view show page

--- routes/gallery/index.js
+++ routes/gallery/index.js
@@ -75,7 +75,7 @@ router.post("/", middleware.isLoggedIn, upload.array("img", 40), async function(
 
 // SHOW
 router.get("/:id", function(req, res){
-	Gallery.findById(req.params.id, function(err, gallery){
+	Gallery.findById(req.params.id).populate({ path: "imgs", populate: {path: "likes"} }).exec(function(err, gallery){
 		if(err){
 			console.log(err);
 			req.flash("error", "Album not found");
