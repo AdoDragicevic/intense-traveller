55da24c228f95bf6cb98165b20e3fafd64ca21f8
Ado Dragicevic <adodragicevic@gmail.com>
2020-09-16 15:47:03 +0000

    add changes to backend

--- routes/gallery/like.js
+++ routes/gallery/like.js
@@ -7,33 +7,35 @@ const middleware = require("../../middleware");
 
 
 // Gallery LIKE route
-router.post("/", middleware.isLoggedIn, function(req, res){
+router.post("/:img_id", middleware.isLoggedIn, function(req, res){
 	Gallery.findById(req.params.id, function(err, gallery){
 		if(err || !gallery){
 			console.log(err);
 			req.flash("error", "Ubable to like, please try again later.");
 			res.redirect("back");
 		}else{
-			// check if req.user._id exists in gallery.likes
-			let foundUserLike = gallery.likes.some(function(like){
-				return like.equals(req.user._id);
-			});
-			if(foundUserLike){
-				// user already liked, removing like
-				gallery.likes.pull(req.user._id);
-			}else{
-				// add new user like to blog.likes
-				gallery.likes.push(req.user);
-			}
-			gallery.save(function(err, gallery){
-				if(err){
-					console.log(err);
-					req.flash("error", "Something went wrong, please try again later.");
-					res.redirect("back");
+			gallery.imgs.forEach(function(img){
+				// check if req.user._id exists in gallery.likes
+				let foundUserLike = img.likes.some(function(like){
+					return like.equals(req.user._id);
+				});
+				if(foundUserLike){
+					// user already liked, removing like
+					img.likes.pull(req.user._id);
 				}else{
-					res.redirect("/gallery/" + gallery._id);
+					// add new user like to blog.likes
+					img.likes.push(req.user);
 				}
 			});
+			gallery.save(function(err, gallery){
+					if(err){
+						console.log(err);
+						req.flash("error", "Something went wrong, please try again later.");
+						res.redirect("back");
+					}else{
+						res.redirect("/gallery/" + gallery._id + "/" + req.params.img_id);
+					}
+				});
 		}
 	});	
 });
